5e08ffec9604d4a3830fde8e2d003176d8923679
unknown <bhatraprerna061@gmail.com>
2021-01-06 12:04:19 +0530

    rd day

--- medium/script.js
+++ medium/script.js
@@ -34,7 +34,7 @@ function selectableTextAreaMouseUp(event) {
 function showdiv()
 {
 
-	 const selectedText = window.getSelection().toString().trim();
+	 const selectedText = document.getSelection().toString().trim();
  	//	alert(selectedText)
    // documentMouseDown()
  	var highlight= document.createElement('div');
@@ -65,54 +65,96 @@ function showdiv()
        document.getElementById("Div1").style.right=0;
         document.getElementById("Div1").style.top=0;
        document.getElementById("Div1").style.padding='20px 20px 20px 20px';
-        var pos= window.getSelection().getRangeAt(0).getBoundingClientRect()
+        var pos= document.getSelection().getRangeAt(0).getBoundingClientRect()
+        let RangeOfText=document.getSelection().getRangeAt(0)
         console.log(pos)
+        console.log("Range Of Text")
+        console.log(RangeOfText)
         document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
   
-
- //  documentMouseDown()  
- //document.addEventListener("mousedown", documentMouseDown);
-
-
-
  document.getElementById("commentbtn").addEventListener("click",function()
  {
-   publish(pos)
+   publish(pos,RangeOfText)
  });
 }
 
-function publish(cordinators)
+function publish(cordinators,RangeOfText)
 {
  // alert("gdd")
+ console.log("cordinators in publish")
+ console.log(cordinators)
+ console.log("RangeOfText in publish")
+ console.log(RangeOfText)
+ console.log(typeof(RangeOfText))
  var inputval=document.getElementById("commentinput").value;
 // alert(inputval)
   console.log("inputval on publish"+inputval)
-
+  const RangeObje={}
+  RangeObje.r=RangeOfText;
+  console.log(RangeObje)
   if(localStorage.getItem('comments'))
   {
-
     const comments1= JSON.parse(localStorage.getItem('comments'))
     console.log(comments1)
     //var commentsarray=Object.values(comments1)
-    console.log(typeof(comments1))
-    comments1.push({"comments":inputval,"poss":cordinators.y})
+    console.log(typeof(cordinators))
+    comments1.push({"comments":inputval,"poss":cordinators.y,"start":RangeOfText.startOffset,"end":RangeOfText.endOffset})
+      console.log("comments1 array,Rnage is shown here bt not saved in localstorge")
+      console.log(comments1)
       localStorage.setItem("comments", JSON.stringify(comments1));
-    console.log(comments1)
+
   }
 else{
+
+    
   const comments=[]
-   comments.push({"comments":inputval,"poss":cordinators.y})
+   console.log(typeof(cordinators))
+   comments.push({"comments":inputval,"poss":cordinators.y,"start":RangeOfText.startOffset,"end":RangeOfText.endOffset})
    localStorage.setItem("comments", JSON.stringify(comments));
    console.log(comments)
 }  
 }
 
-function scroll(param)
+function scroll(param,param1,param2)
 {
- console.log(param)
+  //console.log("Range on scroll"+param1+"typeOf"+typeof(param1))
+  const ele=document.getElementById("MainPara")
+  const textNode = ele.childNodes[0];
+  console.log(param1)
+ console.log(typeof(param))
+  console.log(param2)
+ console.log(typeof(param2))
  window.scrollTo(0, param);
-//window.scrollTo(0, pos.y);
+
+ const range = document.createRange();
+range.setStart(textNode, param1);
+range.setEnd(textNode, param2);
+const mark = document.createElement('mark');
+range.surroundContents(mark);
+
+ 
+}
+
+function markNode() {
+if(document.getSelection() && document.getSelection().toString().length){
+let range = document.getSelection().getRangeAt(0);
+let newNode = document.createElement('mark');
+      newNode.appendChild(range.extractContents());
+      range.insertNode(newNode);
+}
+else{
+alert('please make selection of text to mark');
+}
+}
+
+
+
+function highlight(element,st,en)
+{
+ // alert(st)
+  //document.getElementById('MainPara').style.color='pink'
+
 }
 
 function ShowComments()
@@ -128,12 +170,17 @@ function ShowComments()
       console.log(CommentArray)
 
       const CommentPossiitons=[]
+      const CommentRangeStart=[]
+      const CommentRangeEnd=[]
       for(let i=0;i<CommentArray.length;i++)
       {
+        CommentRangeStart.push(CommentArray[i].start)
+        CommentRangeEnd.push(CommentArray[i].end)
+
           var CommentDiv=document.createElement('div')
           document.getElementById('CommentBody').appendChild(CommentDiv)
           var CommentInput=document.createElement('INPUT')
-          CommentDiv.setAttribute("class","Comment")
+              CommentDiv.setAttribute("class","Comment")
           document.getElementById('CommentBody').appendChild(CommentInput)
            CommentInput.setAttribute("class","CommentInputId")
             CommentInput.setAttribute("placeholder",CommentArray[i].poss)
@@ -147,11 +194,12 @@ function ShowComments()
           document.getElementsByClassName("Comment")[i].innerHTML=CommentArray[i].comments
           document.getElementsByClassName("Comment")[i].addEventListener("click",function() {
             console.log(i)
-            scroll(CommentPossiitons[i])
+            scroll(CommentPossiitons[i],CommentRangeStart[i],CommentRangeEnd[i])
           })
 
       }
            console.log(CommentPossiitons)
+         //  console.log("Range of Comment"+JSON.stringify(CommentRange))
   }
   else
   {
