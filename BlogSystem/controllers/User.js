91bb46aaaec1f2e7c50f3369be36ca33964825cd
unknown <bhatraprerna061@gmail.com>
2021-01-09 11:38:12 +0530

    config

--- BlogSystem/controllers/User.js
+++ BlogSystem/controllers/User.js
@@ -78,5 +78,9 @@ exports.signin=(req,res)=>
 	})
 }
 
-
+exports.signout=(req,res)=>
+{
+	res.clearCookie('t')
+	res.json({message:"Signput success"});
+}
 
--- BlogSystem/routers/User.js
+++ BlogSystem/routers/User.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const router=express.Router();
 
-const {signup,signin}=require('../controllers/user');
+const {signup,signin,signout}=require('../controllers/user');
 
 router.post("/signup",signup)
-router.get("/signin",signin)
+router.post("/signin",signin)
+router.get("/signout",signout)
 
 module.exports=router;
